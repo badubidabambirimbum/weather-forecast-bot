5fbb92e706a5a317cfb20a4ae4809d1ac0f5df7e
badubidabambirimbum <chetvergovalex@mail.ru>
2024-09-06 21:07:23 +0300

    update 3.3.1

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -3,8 +3,9 @@ from auth_data import token  # API KEY
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from Keyboards import kb, kb_help, kb_cities, ikb_info
-import pandas as pd
 from datetime import datetime
+import pandas as pd
+import asyncio
 
 import sys
 import os
@@ -71,7 +72,12 @@ TRANSLATE_CITIES = {"Москва": "Moscow",
 bot = Bot(token)
 dp = Dispatcher(bot)
 table = table()
-scheduler = AsyncIOScheduler()
+
+scheduler_async = AsyncIOScheduler()
+
+all_users_id = pd.read_csv('users_data/all_users_id.csv')
+add_users_id = pd.read_csv('users_data/add_users_id.csv')
+
 
 def create_forecast(city, dist, period):
     forecast_Yandex = table.datasets[TRANSLATE_CITIES[city]]["Yandex"].iloc[-1]
@@ -83,7 +89,8 @@ def create_forecast(city, dist, period):
 
     for i in range(1, int(dist) + 1):
         date = future_dates[i - 1]
-        if forecast_Yandex[f'weather{i}'] in WEATHER_YANDEX_SMILE and forecast_GisMeteo[f'weather{i}'] in WEATHER_GISMETEO_SMILE:
+        if (forecast_Yandex[f'weather{i}'] in WEATHER_YANDEX_SMILE and
+                forecast_GisMeteo[f'weather{i}'] in WEATHER_GISMETEO_SMILE):
             if WEATHER_YANDEX_SMILE[forecast_Yandex[f'weather{i}']] == WEATHER_GISMETEO_SMILE[forecast_GisMeteo[f'weather{i}']]:
                 forecast_data += (f"\n"
                                   f"✨ {date.strftime('%Y-%m-%d')} ✨\n"
@@ -106,8 +113,6 @@ def create_forecast(city, dist, period):
 
 
 async def scheduled_notification():
-    add_users_id = pd.read_csv('users_data/add_users_id.csv')
-
     for user in add_users_id.itertuples():
         try:
             await bot.send_message(user.id, text=create_forecast(user.city, 1, 1), parse_mode='HTML')
@@ -115,9 +120,24 @@ async def scheduled_notification():
             print(f"Не удалось отправить сообщение пользователю {user.id}: {e}")
 
 
-def start_scheduler():
-    scheduler.add_job(scheduled_notification, 'cron', hour=12, minute=29)
-    scheduler.start()
+async def update_dataset(city, type):
+    try:
+        table.update(city, type)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        await asyncio.sleep(1800)
+        await update_dataset(city, type)
+
+
+def start_scheduler_async():
+    scheduler_async.add_job(scheduled_notification, 'cron', hour=6, minute=0)
+    scheduler_async.add_job(update_dataset, 'cron', hour=5, minute=0, args=["Moscow", "GisMeteo"])
+    scheduler_async.add_job(update_dataset, 'cron', hour=5, minute=1, args=["Krasnodar", "Yandex"])
+    scheduler_async.add_job(update_dataset, 'cron', hour=5, minute=2, args=["Ekaterinburg", "GisMeteo"])
+    scheduler_async.add_job(update_dataset, 'cron', hour=5, minute=3, args=["Moscow", "Yandex"])
+    scheduler_async.add_job(update_dataset, 'cron', hour=5, minute=4, args=["Krasnodar", "GisMeteo"])
+    scheduler_async.add_job(update_dataset, 'cron', hour=5, minute=5, args=["Ekaterinburg", "Yandex"])
+    scheduler_async.start()
 
 
 async def on_startup(_):
@@ -130,15 +150,15 @@ async def on_shutdown(_):
 
 @dp.message_handler(commands=["start"])
 async def start_message(message: types.Message):
-    all_users_id = pd.read_csv('users_data/all_users_id.csv')
+    global all_users_id
     user_id = message.from_user.id
 
     if user_id not in all_users_id['id'].values:
         new_user = pd.DataFrame({"id": [user_id],
-                    "date": [datetime.now().strftime('%Y-%m-%d')]})
+                                 "date": [datetime.now().strftime('%Y-%m-%d')]})
 
         all_users_id = pd.concat([all_users_id, new_user], ignore_index=True)
-        all_users_id.to_csv(f'all_users_id.csv')
+        all_users_id.to_csv(f'users_data/all_users_id.csv')
         all_users_id.to_excel(f'users_data/all_users_id.xlsx')
 
         print(f"Новый пользователь: {user_id}!!!")
@@ -157,9 +177,10 @@ async def help_message(message: types.Message):
     await bot.send_sticker(message.from_user.id,
                            sticker="CAACAgIAAxkBAAEMj1Fmp6-tcw1DpXSWJp3yCkcgTFAy6QACshIAAmD9iUtRNBJT06z1kDUE",
                            reply_markup=ReplyKeyboardRemove())
-    await message.reply(text='Введи название города🏙 и выбери количество дней📅 для получения прогноза в этом городе на выбранный период!😉',
-                        parse_mode='HTML',
-                        reply_markup=kb_cities)
+    await message.reply(
+        text='Введи название города🏙 и выбери количество дней📅 для получения прогноза в этом городе на выбранный период!😉',
+        parse_mode='HTML',
+        reply_markup=kb_cities)
 
 
 @dp.message_handler(commands=["info"])
@@ -196,13 +217,13 @@ async def weather_message(message: types.Message):
 
 @dp.message_handler(commands=["add"])
 async def add_message(message: types.Message):
+    global add_users_id
     command, *args = message.text.split()
     if args:
         city = ' '.join(args)
         if city not in SET_CITIES:
             await message.reply("После команды /add должно быть одно слово - доступный город🏙")
         else:
-            add_users_id = pd.read_csv('users_data/add_users_id.csv')
             user_id = message.from_user.id
             if user_id not in add_users_id['id'].values:
                 new_user = pd.DataFrame({"id": [user_id],
@@ -224,7 +245,7 @@ async def add_message(message: types.Message):
 
 @dp.message_handler(commands=["remove"])
 async def remove_message(message: types.Message):
-    add_users_id = pd.read_csv('users_data/add_users_id.csv')
+    global add_users_id
     user_id = message.from_user.id
 
     if user_id in add_users_id['id'].values:
@@ -270,5 +291,5 @@ async def callback_message(callback: types.CallbackQuery):
 
 
 if __name__ == "__main__":
-    start_scheduler()
+    start_scheduler_async()
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True, on_shutdown=on_shutdown)
