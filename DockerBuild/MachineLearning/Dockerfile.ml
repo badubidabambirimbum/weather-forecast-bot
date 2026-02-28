e33b86e8b3e2296b8bba36ac8c3c44f094c59dbb
Alexey Chetvergov <chetvergovalex@mail.ru>
2026-02-28 16:14:52 +0300

    оптимизировал сборку контейнеров

--- DockerBuild/MachineLearning/Dockerfile.ml
+++ DockerBuild/MachineLearning/Dockerfile.ml
@@ -3,14 +3,12 @@ FROM python:3.11
 WORKDIR /app/machine_learning
 
 COPY DockerBuild/MachineLearning/requirements.txt .
-COPY app/src/machine_learning /app/machine_learning
-COPY app/src/core /app/core
-
 RUN python -m pip install --upgrade pip && pip install -r requirements.txt
 RUN rm requirements.txt
 
 ENV PYTHONUNBUFFERED=1
 ENV TZ=Europe/Moscow
-
 ENV PYTHONPATH=/app
-RUN echo $PYTHONPATH
+
+COPY app/src/machine_learning /app/machine_learning
+COPY app/src/core /app/core
